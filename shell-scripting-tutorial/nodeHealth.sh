39f9d33a8911901edd50706efdf319c57a2235ee
Vineet Pachouri <93171434+Vineet140920@users.noreply.github.com>
2026-06-21 08:18:35 +0000

    added

---
+++ shell-scripting-tutorial/nodeHealth.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+###################
+# Author: Vineet
+# Date: 21/06/2026
+#
+# This script outputs to  node health
+#
+# version : V1
+# ##################################
+
+
+set -x  # debug mode 
+
+df -h
+
+
+free -g
+
+
+
+nproc
