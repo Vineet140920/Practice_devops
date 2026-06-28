4df1bf5e1bd1c17f7536af6e49232f7b41797250
Vineet Pachouri <93171434+Vineet140920@users.noreply.github.com>
2026-06-28 06:18:48 +0000

    added

---
+++ shell-scripting-projects/User_information_collector/user_info.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "User Information"
+
+read -p "Enter Your name: " name
+
+read -p "Enter Your age: " age
+
+
+read -p "Enter Your city:" city
+
+read -p "Enter Your company:" company
+
+
+echo "User Details"
+
+echo "Name": $name
+echo "age": $age
+echo "city": $city
+echo "company": $company
+
+echo 
+echo "Thank You"
