87cdd8a217f41c8eeece7b3cb7476203ac8441f2
Vineet Pachouri <93171434+Vineet140920@users.noreply.github.com>
2026-06-28 06:02:49 +0000

    project added

---
+++ shell-scripting-projects/Hello_World_Automation/hello_world.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Hello Vineet!"
+
+echo "Welocme to shell-scripting"
+
+
+echo "Current User: $( whoami)"
+
+echo "Today's Date:  $( date)"
+
+echo "Current Directory:$(pwd)"
+
+
+echo "Thankyou for running this script!"
+
+echo "====================================="
